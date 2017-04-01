9ff4486aa793e7bcf726f6b2a0edd111d1907e3c
Tom Joy <tomjoy002@gmail.com>
2017-04-01 23:10:36 +0530

    Added a Procfile.

--- mysites/settings.py
+++ mysites/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
+WSGI_APPLICATION = 'indianstockideas.wsgi.application'
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
