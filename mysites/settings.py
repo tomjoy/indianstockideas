e3060ff34153bb296d278d1c031945379f4c7cf4
Tom Joy <tomjoy002@gmail.com>
2017-04-01 22:52:18 +0530

    Added a Procfile.

--- mysites/settings.py
+++ mysites/settings.py
@@ -143,6 +143,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 LOGIN_URL = '/login/'
