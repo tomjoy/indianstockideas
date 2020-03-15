098bdaf0db17ae5faa7a2b8ed488a5b61f61d7c1
Tom Joy <tomjoy002@gmail.com>
2020-03-15 16:19:57 +0530

    screener changes

--- indianstockideas/views.py
+++ indianstockideas/views.py
@@ -658,7 +658,7 @@ def getscreener():
     doc = html.fromstring(result.text)
     #import pdb;pdb.set_trace() 
     
-    pages = doc.xpath("//div[@class='flexed']//text()")
+    pages = doc.xpath("//div[@class='flexed']")
     print pages
     print "###########################################################3"
     return str(pages)
